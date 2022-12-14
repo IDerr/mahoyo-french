2c23963d6348f50d75155512aeba7630a95cbf15
IDerr <ibrahim@derraz.fr>
2022-12-14 22:51:32 +0100

    Add patch voice for chapter 1

--- Tools/patch.bat
+++ Tools/patch.bat
@@ -1,3 +1,4 @@
 arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\Patch\patch1" "..\Release\Patch\patch1.xp3" mahoyo
 arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\Patch\patch2" "..\Release\Patch\patch2.xp3" mahoyo
 arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\Patch\patch3" "..\Release\Patch\patch3.xp3" mahoyo
+arc_conv_r54\arc_conv.exe --pack xp3 "..\Release\Patch\patch8" "..\Release\Patch\patch8.xp3" mahoyo
